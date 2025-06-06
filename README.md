ec6f495b3abd8b3f86406b555c71040ac3bfcec1
felipe <felipemiritz@yahoo.com.br>
2025-06-06 17:22:19 -0300

    criada views de alunos

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 2025-1-A3-UDWMJ-FAPA-04
 
 Aplicativo de Gestão Escolar.
-1 - Desenvolvido usando Django Framework e Bootstrap.
-2 - Desenvolvido utilizando Vue.JS e Quasar.
+\n1 - Desenvolvido usando Django Framework e Bootstrap.
+\n2 - Desenvolvido utilizando Vue.JS e Quasar.
 
 
--- escolaapp-django/apps/alunos/views.py
+++ escolaapp-django/apps/alunos/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import AlunoForm
+from .models import Aluno
 
 # Create your views here.
+
+def add_aluno(request):
+    template_name = 'alunos/add_aluno.html'
+    context = {}
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            f = form.save(commit = False)
+            f.save()
+            form.save_m2m()
+            return redirect('alunos:list_alunos')
+    form = AlunoForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+def list_alunos(request):
+    template_name = 'alunos/list_alunos.html'
+    alunos = Aluno.objects.filter()
+    context = {
+        'alunos': alunos
+    }
+    return render(request, template_name, context)
+
+def edit_aluno(request, id_product):
+    template_name = 'alunos/add_aluno.html'
+    context = {}
+    aluno = get_object_or_404(Aluno, id = id_product)
+    if request.method == 'POST':
+        form = AlunoForm(request.POST, instance=aluno)
+        if form.is_valid():
+            form.save()
+            return redirect('alunos:list_alunos')
+    form = AlunoForm(instance = aluno)
+    context['form'] = form
+    return render(request, template_name, context)
+
+def delete_aluno(request, id_aluno):
+    aluno = Aluno.objects.get(id = id_aluno)
+    aluno.delete()
+    return redirect('alunos:list_alunos')
