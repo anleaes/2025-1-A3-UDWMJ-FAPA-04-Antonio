a9755eb0f5d39f7e500acedf5bb56448570a3470
felipe <felipemiritz@yahoo.com.br>
2025-06-07 09:36:40 -0300

    Adicionada as urls no core

--- escolaapp-django/escolaapp/urls.py
+++ escolaapp-django/escolaapp/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
+    path('alunos/', include('alunos.urls', namespace='alunos')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
