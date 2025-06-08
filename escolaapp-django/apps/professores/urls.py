01d6ceb18a8b0c1db851e5934bfecd5f9259c2f6
felipe <felipemiritz@yahoo.com.br>
2025-06-08 17:09:30 -0300

    Correção de erros em urls

--- escolaapp-django/apps/professores/urls.py
+++ escolaapp-django/apps/professores/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'professores'
 
 urlpatterns = [
-    path('', views.list_professores, name='list_professor'),
+    path('', views.list_professores, name='list_professores'),
     path('adicionar/', views.add_professor, name='add_professor'),
     path('editar/<int:id_client>/', views.edit_professor, name='edit_professor'),
     path('excluir/<int:id_client>/', views.delete_professor, name='delete_professor'),
