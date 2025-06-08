049874620b5b603d40c20e39a427db1c89e4113e
felipe <felipemiritz@yahoo.com.br>
2025-06-08 12:24:23 -0300

    Criação do models de especialização

--- escolaapp-django/apps/especializacao/models.py
+++ escolaapp-django/apps/especializacao/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Especializacao(models.Model):
+    created_on = models.DateTimeField(auto_now_add = True)
+    updated_on = models.DateTimeField(auto_now = True)
+    name = models.CharField('Nome', max_length = 50)
+    description = models.TextField('Descricao', max_length = 100) 
+    
+    class Meta:
+        verbose_name = 'Especializacao'
+        verbose_name_plural = 'Especializacoes'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
