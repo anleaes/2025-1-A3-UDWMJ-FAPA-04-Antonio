544be4c75d88d0a1ad1358e927561c6662eaac93
felipe <felipemiritz@yahoo.com.br>
2025-06-18 10:36:12 -0300

    Execução de makemigrations e migrate

--- apps/rooms/models.py
+++ apps/rooms/models.py
@@ -6,7 +6,7 @@ from supplies.models import Supply
 class Room(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    number = models.IntegerField('Numero', max_length=4)
+    number = models.IntegerField('Numero')
     capacity = models.IntegerField('Capacidade')
     supplies = models.ForeignKey(Supply, on_delete=models.CASCADE)
     
