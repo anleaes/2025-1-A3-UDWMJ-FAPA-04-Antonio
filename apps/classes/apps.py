08e440a67d92b05a102087f7b4af86737788c2a2
felipe <felipemiritz@yahoo.com.br>
2025-06-18 17:33:15 -0300

    Configs iniciais do app classes

--- apps/classes/apps.py
+++ apps/classes/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ClassesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'classes'
-    verbose_name = 'Curso'
+    verbose_name = 'Cursos'
